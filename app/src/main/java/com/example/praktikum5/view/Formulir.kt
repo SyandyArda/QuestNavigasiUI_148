8518fc928bb292c275a3456f33f86f38d609106d
Arsyan <syandi.2901@gmail.com>
2025-11-06 00:55:10 -0800

    menambahkan HorizontalDivider Isi Ruang

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -48,6 +48,9 @@ fun FormIsian(
                 onValueChange = {}
             )
             HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp), thickness = Thickness, color =
+                Color.Red)
             )
         }
     }
