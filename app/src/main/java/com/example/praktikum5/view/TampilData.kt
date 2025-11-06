bbb1032888ee936b4aa5be5602b314e2e78774c3
Arsyan <syandi.2901@gmail.com>
2025-11-06 01:22:36 -0800

    menambahkan val dan Scaffold awal Isi Ruang di tampildata

--- app/src/main/java/com/example/praktikum5/view/TampilData.kt
+++ app/src/main/java/com/example/praktikum5/view/TampilData.kt
@@ -1,2 +1,41 @@
 package com.example.praktikum5.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.praktikum5.R
+
+@Composable
+fun TampilData(
+    onBackBtnClick:()->Unit
+){
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id = "ALAMAT"), second = "Yogyakarta")
+    )
+    Scaffold (modifier = Modifier,
+        {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id=R.string.tampil),
+                        color = Color.White
+                    )},
+                colors = TopAppBarDefaults
+                    .mediumTopAppBarColors(colorResource
+                        (id = R.color.teal_700)
+                    )
+            )
+        }){     isiRuang->
+        Column {  }
+
+    }
+}
