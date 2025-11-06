8eb87467017419342dda8a2ac4fb62758c3672d6
Arsyan <syandi.2901@gmail.com>
2025-11-06 02:09:41 -0800

    mengubah main

--- app/src/main/java/com/example/praktikum5/MainActivity.kt
+++ app/src/main/java/com/example/praktikum5/MainActivity.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier // Pastikan Modifier di-import
 import com.example.praktikum5.ui.theme.Praktikum5Theme
+// BENAR: Impor Composable 'DataApp' dari file Navigasi.kt Anda
+import com.example.praktikum5.DataApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +15,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Praktikum5Theme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+                DataApp(modifier = Modifier)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Praktikum5Theme {
-        Greeting("Android")
     }
 }
