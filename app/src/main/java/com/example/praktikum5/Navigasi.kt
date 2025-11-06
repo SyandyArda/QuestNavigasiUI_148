006973a5ec2e039c0d10b4f9455a9e35bcb3af1d
Arsyan <syandi.2901@gmail.com>
2025-11-06 01:45:35 -0800

    menambahkan scaffold di navigation

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -1,10 +1,15 @@
 package com.example.praktikum5
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.praktikum5.view.FormIsian
 
 enum class Navigasi {
     Formulirku,
@@ -15,4 +20,18 @@ enum class Navigasi {
 fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
-){}
+){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian (
+
+                )
+            }
+        }
+    }
+}
