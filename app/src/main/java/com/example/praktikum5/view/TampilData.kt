9cab8647f8d4a46e7f008e96806f7547d08ba1a8
Arsyan <syandi.2901@gmail.com>
2025-11-06 01:27:27 -0800

    menambahkan Column Isi Ruang di tampildata

--- app/src/main/java/com/example/praktikum5/view/TampilData.kt
+++ app/src/main/java/com/example/praktikum5/view/TampilData.kt
@@ -1,6 +1,9 @@
 package com.example.praktikum5.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -9,9 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import com.example.praktikum5.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TampilData(
     onBackBtnClick:()->Unit
@@ -35,7 +41,24 @@ fun TampilData(
                     )
             )
         }){     isiRuang->
-        Column {  }
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.padding(dimensionResource(id=R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource
+                    (R.dimen.padding_small))
+            ) {
+                items.forEach { items ->
+                    Column {
+                        Text(text = item.first.uppercase(),
+                            fontSize = 16.sp)
+                        Text()
+                    }
+                }
+            }
+        }
 
     }
 }
