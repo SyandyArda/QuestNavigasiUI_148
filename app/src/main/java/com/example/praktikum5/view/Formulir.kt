d93b8e96e7fe7fb60ddebb675704c12fa72f33c8
Arsyan <syandi.2901@gmail.com>
2025-11-06 00:34:59 -0800

    menambahkan Form isian serta scaffold awalannya

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -1,2 +1,18 @@
 package com.example.praktikum5.view
 
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-Laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+){
+    Scaffold (modifier= Modifier,
+        topBar={
+            TopAppBar(
+                title
+            )
+        })
+}
