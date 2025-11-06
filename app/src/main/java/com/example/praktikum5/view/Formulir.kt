982c97bcfc40864272f6286e31369d9245f37df7
Arsyan <syandi.2901@gmail.com>
2025-11-06 00:46:16 -0800

    menambahkan Column Isi Ruang

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -1,11 +1,20 @@
 package com.example.praktikum5.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import com.example.praktikum5.R
 
 @Composable
 fun FormIsian(
@@ -20,5 +29,15 @@ fun FormIsian(
                 colors = TopAppBarDefaults.topAppBarColors
                     (colorResource(id = R.color.teal_700))
             )
-        })
+        }
+    ){ isiRuang ->
+        Column(modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+
+            )
+        }
+    }
 }
